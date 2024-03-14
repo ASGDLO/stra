8364b2144498567ab1ecd09d6c96642aa9ab928c
Ceyhan Molla <ceyhanmolla@users.noreply.github.com>
2024-03-14 22:47:07 +0300

    Hyperopt

--- ROTT.py
+++ ROTT.py
@@ -1,43 +1,50 @@
 from freqtrade.strategy.interface import IStrategy
 from pandas import DataFrame
+import numpy as np
+import pandas as pd
 import talib.abstract as ta
+from freqtrade.strategy import DecimalParameter, IntParameter
 
 class ROTT(IStrategy):
-    """
-    Hareketli Ortalamalar ve OTT Göstergesi kullanarak alım ve satım sinyalleri üreten strateji.
-    """
-    # Strateji parametreleri
+    # Strategy parameters
     timeframe = '1m'
-    stoploss = -0.1  # Stop loss oranı, %10 olarak belirlendi.
-    
+    stoploss = -0.1
+    minimal_roi = {
+        "60": 0.01
+    }
+
+    # Parameters for 'buy' space
+    x1 = IntParameter(5, 50, default=30, space='buy')
+    x2 = IntParameter(50, 1500, default=1000, space='buy')
+
+    # Parameters for 'sell' space
+    sell_x1 = IntParameter(5, 50, default=20, space='sell')
+    sell_x2 = IntParameter(50, 1500, default=800, space='sell')
+
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Hareketli Ortalamalar
-        x1 = 30
-        x2 = 1000
-        dataframe['MA_short'] = ta.MA(dataframe, timeperiod=x1, matype=0)
-        dataframe['MA_long'] = ta.MA(dataframe, timeperiod=x2, matype=0)
-        
-        # OTT Göstergesi hesaplama (burada basitleştirilmiş bir versiyon kullanılacaktır)
+        # Moving Averages
+        dataframe['MA_short'] = ta.MA(dataframe, timeperiod=self.x1.value, matype=0)
+        dataframe['MA_long'] = ta.MA(dataframe, timeperiod=self.x2.value, matype=0)
+        dataframe['MA_short_sell'] = ta.MA(dataframe, timeperiod=self.sell_x1.value, matype=0)
+        dataframe['MA_long_sell'] = ta.MA(dataframe, timeperiod=self.sell_x2.value, matype=0)
+
+        # Simplified OTT Indicator calculation
         dataframe['OTT'] = ((dataframe['MA_short'] - dataframe['MA_long']) * 2) + dataframe['MA_long']
-        
+
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Alım koşulları
+        # Buy conditions
         dataframe.loc[
-            (
-                dataframe['MA_short'] > dataframe['OTT']
-            ),
+            (dataframe['MA_short'] > dataframe['OTT']),
             'buy'] = 1
 
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        # Satım koşulları
+        # Sell conditions based on 'sell' parameters
         dataframe.loc[
-            (
-                dataframe['MA_short'] < dataframe['OTT']
-            ),
+            (dataframe['MA_short_sell'] < dataframe['MA_long_sell']),
             'sell'] = 1
 
         return dataframe
